2242f961c9acc820fc3d11f19be381f7718a4f67
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2021-12-29 12:37:52 -0700

    fix permissions

--- install.sh
+++ install.sh
@@ -100,11 +100,11 @@ sudo mount $efiName /mnt/boot
 sudo nixos-generate-config --root /mnt
 
 # Edit Language and Time Zone
-sed -i 's/# time.timeZone = "Europe/Amsterdam"/time.timeZone = "America/Denver"/g' /mnt/etc/nixos/configuration.nix
-sed -i 's/# i18n.defaultLocalte = "en_US.UTF=8"/i18n.defaultLocalte = "en_US.UTF=8"/g' /mnt/etc/nixos/configuration.nix
-sed -i 's/# console = {/console = {/g' /mnt/etc/nixos/configuration.nix
-sed -i 's/# font = "Lat2-Terminus16";/font = "Lat2-Terminus16";/g' /mnt/etc/nixos/configuration.nix
-sed -i 's/# keyMap/keyMap = {/g' /mnt/etc/nixos/configuration.nix
+sudo sed -i 's/# time.timeZone = "Europe/Amsterdam"/time.timeZone = "America/Denver"/g' /mnt/etc/nixos/configuration.nix
+sudo sed -i 's/# i18n.defaultLocalte = "en_US.UTF=8"/i18n.defaultLocalte = "en_US.UTF=8"/g' /mnt/etc/nixos/configuration.nix
+sudo sed -i 's/# console = {/console = {/g' /mnt/etc/nixos/configuration.nix
+sudo sed -i 's/# font = "Lat2-Terminus16";/font = "Lat2-Terminus16";/g' /mnt/etc/nixos/configuration.nix
+sudo sed -i 's/# keyMap/keyMap = {/g' /mnt/etc/nixos/configuration.nix
 
 # Generate fstab file.
 # genfstab -U /mnt >> /mnt/etc/fstab
