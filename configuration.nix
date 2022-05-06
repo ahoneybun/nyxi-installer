e1916c73047a397864b1ad53d83f51b69d54909c
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2022-05-06 16:30:24 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -32,9 +32,9 @@
 
   };
 
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
     defaultLocale = "en_US.UTF-8";
   };
   
