1b799fc5a6d2633d42ec0cc25eabf01f28321575
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2022-05-22 04:08:04 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -94,12 +94,12 @@ sudo nixos-generate-config --root /mnt
 
 curl https://gitlab.com/ahoneybun/nyxi-installer/-/raw/main/config.nix > configuration.nix; sudo mv -f configuration.nix /mnt/etc/nixos/
 
-# Install
-sudo nixos-install
-
 # Replacing username
 sed -i 's/aaronh/$userName/g' /mnt/etc/configuration.nix
 sed -i 's/home/aaronh/home/$userName/g' /mnt/etc/configuration.nix
+
+# Install
+sudo nixos-install
 
 # Start Setup section
 # sudo -i
