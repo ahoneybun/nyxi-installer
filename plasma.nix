e8e084d1d93189ef198f2f430acea2aa3abcdc36
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2022-05-31 13:33:52 +0000

    Update plasma.nix

--- plasma.nix
+++ plasma.nix
@@ -6,4 +6,15 @@
     services.xserver.displayManager.sddm.enable = true;
     services.xserver.desktopManager.plasma5.enable = true;
 
+
+    # Install some packages
+    environment.systemPackages = 
+            with pkgs; 
+            [
+                libsForQt5.bismuth
+                libsForQt5.kde-gtk-config
+                libsForQt5.plasma-nm
+                libsForQt5.plasma-pa
+                libsForQt5.sddm
+            ];
 }
