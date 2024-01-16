9ff54a086411af1058a851ff42ed9a5c1576940a
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2024-01-16 17:47:02 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -74,11 +74,11 @@ elif [ $deviceChoice = 6 ]; then
    sudo sed -i "11 i \           ./darp9.nix" /mnt/etc/nixos/configuration.nix
 
 elif [ $deviceChoice = 7 ]; then
-   curl https://gitlab.com/ahoneybun/nix-configs/-/raw/flake/systems/flake.nix > flake.nix; sudo mv -f flake.nix /mnt/etc/nixos/
+   curl https://gitlab.com/ahoneybun/nix-configs/-/raw/flake/flake.nix > flake.nix; sudo mv -f flake.nix /mnt/etc/nixos/
    sudo nixos-install --flake /mnt/etc/nixos#nixos
 
 elif [ $deviceChoice = 8 ]; then
-   curl https://gitlab.com/ahoneybun/nix-configs/-/raw/flake/systems/flake.nix > flake.nix; sudo mv -f flake.nix /mnt/etc/nixos/
+   curl https://gitlab.com/ahoneybun/nix-configs/-/raw/flake/flake.nix > flake.nix; sudo mv -f flake.nix /mnt/etc/nixos/
    curl https://gitlab.com/ahoneybun/nix-configs/-/raw/main/desktops/gnome.nix > gnome.nix; sudo mv -f gnome.nix /mnt/etc/nixos/
    sudo nixos-install --flake /mnt/etc/nixos#dev-one
 
